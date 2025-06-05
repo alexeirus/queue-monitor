bab0737ddecb4cdc12028fff7f15fed001bce848
alexeirus <43556265+alexeirus@users.noreply.github.com>
2025-06-05 13:44:00 +1200

    Update app.py
    changed to with safe_globals(safe_globals): otherwise app seem to be running just put out an error for app.py around this variable

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if not os.path.exists(MODEL_PATH) or os.path.getsize(MODEL_PATH) < 10000000:
             f.write(chunk)
 
 # ✅ Load YOLO and Analyzer with safe globals
-with safe_globals(safe_global_list):
+with safe_globals(safe_globals):
     model = YOLO(MODEL_PATH)
     analyzer = QueueAnalyzer(model)
 
